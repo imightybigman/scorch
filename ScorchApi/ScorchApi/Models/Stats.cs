affe7cf3f0b0c9e00df41814ab19195483b44310
jun2theizzle <junnobaka@gmail.com>
2017-09-28 21:10:11 -0500

    spacing!

--- ScorchApi/ScorchApi/Models/Stats.cs
+++ ScorchApi/ScorchApi/Models/Stats.cs
@@ -4,25 +4,24 @@ namespace ScorchApi.Models
 {
     public class Stats
     {
-        public int Strength { get; set; }
-        public int Dexterity { get; set; }
+        public int Strength     { get; set; }
+        public int Dexterity    { get; set; }
         public int Constitution { get; set; }
         public int Intelligence { get; set; }
-        public int Wisdom { get; set; }
-        public int Charisma { get; set; }
+        public int Wisdom       { get; set; }
+        public int Charisma     { get; set; }
 
         public Stats() { }
 
         public Stats(DynamoDBEntry entry)
         {
-            var doc = entry.AsDocument();
-
-            Strength = doc["Strength"].AsInt();
-            Dexterity = doc["Dexterity"].AsInt();
-            Constitution = doc["Constitution"].AsInt();
-            Intelligence = doc["Intelligence"].AsInt();
-            Wisdom = doc["Wisdom"].AsInt();
-            Charisma = doc["Charisma"].AsInt();
+            var doc         = entry.AsDocument();
+            Strength        = doc["Strength"].AsInt();
+            Dexterity       = doc["Dexterity"].AsInt();
+            Constitution    = doc["Constitution"].AsInt();
+            Intelligence    = doc["Intelligence"].AsInt();
+            Wisdom          = doc["Wisdom"].AsInt();
+            Charisma        = doc["Charisma"].AsInt();
         }
     }
 }
