6dada606ae97d56376e787821fbbb13783110e50
jun2theizzle <junnobaka@gmail.com>
2017-10-02 20:15:11 -0500

    added inventory to character

--- ScorchApi/ScorchApiV2/Models/Character.cs
+++ ScorchApi/ScorchApiV2/Models/Character.cs
@@ -1,26 +1,28 @@
 ﻿using System.Collections.Generic;
+using ScorchApiV2.Interfaces;
 
 namespace ScorchApiV2.Models
 {
     public class Character
     {
-        public string Firstname     { get; set; }
-        public string Lastname      { get; set; }
-        public string Race          { get; set; }
-        public string Class         { get; set; }
-        public string Sex           { get; set; }
-        public int Age              { get; set; }
-        public string Align         { get; set; }
-        public int Hp               { get; set; }
-        public int MaxHp            { get; set; }
-        public int Gold             { get; set; }
-        public int Exp              { get; set; }
-        public Stats Stats          { get; set; }
-        public Equipment Equipment  { get; set; } 
-        public List<Trait> Traits   { get; set; } = new List<Trait>();
-        public List<Skill> Skills   { get; set; } = new List<Skill>();
-        public List<Spell> Spells   { get; set; } = new List<Spell>();
-        public List<string> Notes   { get; set; } = new List<string>();
+        public string Firstname         { get; set; }
+        public string Lastname          { get; set; }
+        public string Race              { get; set; }
+        public string Class             { get; set; }
+        public string Sex               { get; set; }
+        public int Age                  { get; set; }
+        public string Align             { get; set; }
+        public int Hp                   { get; set; }
+        public int MaxHp                { get; set; }
+        public int Gold                 { get; set; }
+        public int Exp                  { get; set; }
+        public Stats Stats              { get; set; }
+        public Equipment Equipment      { get; set; } 
+        public List<Trait> Traits       { get; set; } = new List<Trait>();
+        public List<Skill> Skills       { get; set; } = new List<Skill>();
+        public List<Spell> Spells       { get; set; } = new List<Spell>();
+        public List<IItem> Inventory    { get; set; } = new List<IItem>();
+        public List<string> Notes       { get; set; } = new List<string>();
         
         public Character() { }
 
