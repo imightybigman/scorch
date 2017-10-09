03b76faf7ab90455e27338873c4b827eb1cfbdf4
jun2theizzle <junnobaka@gmail.com>
2017-10-08 22:55:13 -0500

    added tabs

---
+++ ScorchApp/src/components/DungeonMasterMain.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="dm-main-component">
+        This is your's Jakub
+    </div>
+</template>
+
+<script>
+
+export default {
+    name : 'dm-main-component'
+}
+</script>
+
+<style scoped>
+
+</style>
