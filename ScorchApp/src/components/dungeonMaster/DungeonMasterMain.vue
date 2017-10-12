daf2e37ee966143023add233f35d2d8c8acae32d
Jakub Wlodarczyk <wlodarczykj1027@gmail.com>
2017-10-11 23:40:46 -0500

    Began working on the DM page
    Added Notes and Character tab. Basic styling and basic stuff

--- ScorchApp/src/components/dungeonMaster/DungeonMasterMain.vue
+++ ScorchApp/src/components/dungeonMaster/DungeonMasterMain.vue
@@ -1,16 +1,77 @@
 <template>
     <div class="dm-main-component">
-        This is your's Jakub
+        <div class="d-flex flex-wrap dm-character-view" v-if="dataDone">
+            <div class="d-flex flex-column dm-party-view">
+                <div class="dm-character-cards-container" v-for="(char, index) in party" :key="index">
+                    <character-tile :character="char"></character-tile>
+                </div>
+            </div>
+            <div class="d-flex character-operations">
+                <span>sup </span>
+            </div>
+        </div>
     </div>
+    
 </template>
 
 <script>
+  import { CharacterTile } from 'components/character'
+  import { CharacterService } from 'services'
+  import sortBy from 'lodash/sortBy'
 
 export default {
-    name : 'dm-main-component'
+    name : 'dm-main-component',
+    
+    data() {
+      return {
+        party: [],
+        dataDone: false
+      }
+    },
+    
+    async beforeRouteUpdate (to, from, next) {
+      await this.loadData();
+      next();
+    },
+    async beforeMount() {
+      await this.loadData();
+    },
+    methods: {
+      async loadData() {
+        const characterSvc = new CharacterService();
+        let myParty = await characterSvc.getCharacters();
+        this.party = sortBy(myParty.body, (x) => x.Firstname);
+        this.dataDone = true;
+      }
+    },
+    components: {
+      CharacterTile
+    }
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+    div {
+        box-sizing: border-box;
+    }
+    
+    .dm-main-component {
+        margin: 1%;
+        height: 600px;
+    }
+    .dm-party-view {
+        margin: 1%;
+        flex: 1;
+        flex-grow: 2;
+        .dm-character-cards-container {
+            margin-bottom: 1%;
+            border-radius: 10px;
+            
+        }
+    }
+    .character-operations {
+        margin: 1%;
+        flex: 1;
+        flex-grow: 5;
+    }
 </style>
---
+++ ScorchApp/src/components/dungeonMaster/DungeonMasterNotes.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="dm-notes-component">
+        <div class="d-flex flex-wrap" v-if="dataDone">
+            <div class="flex-column">
+                Sup
+            </div>
+            <div>
+                Yo Yo Yo
+            </div>
+        </div>
+    </div>
+    
+</template>
+
+<script>
+  import { CharacterTile } from 'components/character'
+  import sortBy from 'lodash/sortBy'
+
+export default {
+    name : 'dm-notes-component',
+    
+    data() {
+      return {
+        dataDone: false
+      }
+    },
+    
+    async beforeRouteUpdate (to, from, next) {
+      await this.loadData();
+      next();
+    },
+    async beforeMount() {
+      await this.loadData();
+    },
+    methods: {
+      async loadData() {
+        this.dataDone = true;
+      }
+    },
+    components: {
+      CharacterTile
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    div {
+        box-sizing: border-box;
+    }
+    .notelet-section{
+        flex: 1;
+        flex-grow: 2;
+    }
+
+</style>
