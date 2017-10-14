6f7ea1898bfcef201b5f68935dd1f8f9ad8dad22
Jakub Wlodarczyk <wlodarczykj1027@gmail.com>
2017-10-13 21:49:50 -0500

    Created Entity Stuff

--- ScorchApi/ScorchApiV2/Models/Chapter.cs
+++ ScorchApi/ScorchApiV2/Models/Chapter.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace ScorchApiV2.Models
 {
@@ -10,8 +11,8 @@ namespace ScorchApiV2.Models
         public string Detail                { get; set; }
         public string Title                 { get; set; }
         public int ChapterNumber            { get; set; }
-        public bool IsPlayerVisible      { get; set; }
-
+        public bool IsPlayerVisible         { get; set; }
+        List<Entity> Entities               { get; set; }
         public Chapter() { }
 
     }
