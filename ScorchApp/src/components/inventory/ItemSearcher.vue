9a307d70973aff2ff5ceedefc0bd01ca55fdb958
Jakub Wlodarczyk <wlodarczykj1027@gmail.com>
2017-10-25 20:54:34 -0500

    Moved Around stuff

--- ScorchApp/src/components/inventory/ItemSearcher.vue
+++ ScorchApp/src/components/inventory/ItemSearcher.vue
@@ -1,39 +1,39 @@
 <template>
     <div class="item-searcher border border-dark">
-            <div class="item-searcher-inner">
-                <div class="d-flex top-bar">
-                    <h4 class="item-searcher-header">Searcher</h4>
-                </div>            
-                <div class="input-group">
-                    <input type="text" class="form-control" id="name" v-model="searchTerm" placeholder="Name To Filter By" autocomplete="off" required="true"/>
-                </div>
-                <div class="search-results table-responsive">
-                    <table id="search-results-table" class="table table-hover table-bordered">
-                        <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th>Type</th>
-                                <th>Class</th>
-                                <th>Slot</th>
-                                <th>AC</th>
-                                <th>Dmg</th>
-                                <th>Cost</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr v-for="(item, index) in searchResults" :key="index" v-on:click="selectItem(item)">
-                                <td>{{item.Name}}</td>
-                                <td>{{item.ItemType}}</td>
-                                <td>{{item.ItemClass}}</td>
-                                <td>{{item.Slot}}</td>
-                                <td>{{item.AC}}</td>
-                                <td>{{item.Damage}}</td>
-                                <td>{{item.Cost}}</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
+        <div class="item-searcher-inner">
+            <div class="d-flex top-bar">
+                <h4 class="item-searcher-header">Searcher</h4>
+            </div>            
+            <div class="input-group">
+                <input type="text" class="form-control" id="name" v-model="searchTerm" placeholder="Name To Filter By" autocomplete="off" required="true"/>
             </div>
+            <div class="search-results table-responsive">
+                <table id="search-results-table" class="table table-hover table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Type</th>
+                            <th>Class</th>
+                            <th>Slot</th>
+                            <th>AC</th>
+                            <th>Dmg</th>
+                            <th>Cost</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="(item, index) in searchResults" :key="index" v-on:click="selectItem(item)">
+                            <td>{{item.Name}}</td>
+                            <td>{{item.ItemType}}</td>
+                            <td>{{item.ItemClass}}</td>
+                            <td>{{item.Slot}}</td>
+                            <td>{{item.AC}}</td>
+                            <td>{{item.Damage}}</td>
+                            <td>{{item.Cost}}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
     
 </template>
