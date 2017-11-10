d7cb8d22e0a6ec9f0d77cc4e1a4d5c201feb60ea
Jakub Wlodarczyk <wlodarczykj1027@gmail.com>
2017-11-10 07:44:04 -0600

    Revert "Merge"
    This reverts commit b4eee42348f0d082d08d0910e73ec176d034cd5f, reversing
    changes made to df66140eaa9a6098bb17badd78f611e3dc3eed5c.

---
+++ ScorchApp/src/components/inventory/Inventory.vue
@@ -0,0 +1,80 @@
+<template>
+<div class="card d-flex inventory">
+  <div class="card-header">
+    Inventory
+  </div>
+  <div class="card-body">
+    <div id="inventory" role="tablist">
+      <weapon-inventory @equip="equipItem" @sell="sellItem" :characterId="characterId" :weapons="weapons"></weapon-inventory>
+      <armor-inventory @equip="equipItem" @sell="sellItem" :armors="armors"></armor-inventory>
+      <accessory-inventory @equip="equipItem" @sell="sellItem" :accessories="accessories"></accessory-inventory>      
+      <quiver-inventory @equip="equipItem" @sell="sellItem" :characterId="characterId" :quivers="quivers"></quiver-inventory>
+      <adventurer-gear-inventory @sell="sellItem" :adventurerGears="adventurerGears"></adventurer-gear-inventory>
+    </div>
+  </div>
+</div>
+
+
+</template>
+
+<script>
+import WeaponInventory from './weapon/WeaponInventory'
+import AdventurerGearInventory from './adventurergear/AdventurerGearInventory'
+import QuiverInventory from './quiver/QuiverInventory'
+import ArmorInventory from './armor/ArmorInventory'
+import AccessoryInventory from './accessory/AccessoryInventory'
+
+import { mapGetters } from 'vuex'
+
+export default {
+    name: 'character-inventory',
+    props: ['characterId'],
+    computed: {
+      weapons() {
+        return this.$store.getters.getCharacterWeapons(this.characterId);
+      },
+      armors() {
+        return this.$store.getters.getCharacterArmors(this.characterId);
+      },
+      quivers() {
+        return this.$store.getters.getCharacterQuivers(this.characterId);
+      },
+      adventurerGears() {
+        return this.$store.getters.getCharacterAdventurerGears(this.characterId);        
+      },
+      accessories(){
+        return this.$store.getters.getCharacterAccessories(this.characterId);        
+      }
+    },
+    methods: {
+      async equipItem(item) {
+        let equipPayload = {
+          characterId: this.characterId,
+          item: item
+        }
+        await this.$store.dispatch('equipItem', equipPayload);
+      },
+      async sellItem(item) {
+        let sellPayload = {
+          characterId: this.characterId,
+          itemId: item.ItemId
+        }
+        await this.$store.dispatch('sellItem', sellPayload);
+      }
+    },
+    components: {
+      WeaponInventory,
+      AdventurerGearInventory,
+      QuiverInventory,
+      ArmorInventory,
+      AccessoryInventory
+    }
+}
+
+</script>
+
+<style lang="scss" scoped>
+.inventory {
+  flex: 1 0 auto;
+}
+</style>
---
+++ ScorchApp/src/components/inventory/adventurergear/AdventurerGearInventory.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="card d-flex inventory-card">
+        <adventurer-gear-details :adventurergear="selectedAdvGear" :showModal="showDetail"  v-on:close="showDetail = false"></adventurer-gear-details>
+        <div class="card-header" role="tab" id="adventurergear">
+            <h5 class="mb-0">
+            <a data-toggle="collapse" href="#adventurerGearInventory" aria-expanded="false" aria-controls="adventurerGearInventory">
+                Adventure Gear
+            </a>
+            </h5>
+        </div>
+        <div id="adventurerGearInventory" class="collapse" role="tabpanel" aria-labelledby="adventurergear" data-parent="#inventory">
+            <div class="card-body item-list">
+                <div v-for="(adventurerGear, index) in adventurerGears" 
+                    :key="index" 
+                    class="d-flex flex-wrap flex-column list-item border">
+                    <div @click="adventurerGearClick(adventurerGear)">
+                        <item-card :item="adventurerGear"></item-card>
+                    </div>
+                    <div>
+                        <button class="btn btn-danger" @click="sellAdventurerGear(adventurerGear)">
+                            Sell
+                        </button>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+import AdventurerGearDetails from './AdventurerGearDetails';
+import { ItemCard } from 'components/items'
+
+export default {
+    name: 'adventurerGear-inventory',
+    data() {
+        return {
+            selectedAdvGear: {},
+            showDetail: false
+        }
+    }, 
+    props: ['adventurerGears'],
+    methods: {
+        adventurerGearClick(adventurerGear) {
+            this.selectedAdvGear = adventurerGear;
+            this.showDetail = true;
+        },
+        sellAdventurerGear(adventurerGear) {
+            this.$emit('sell', adventurerGear);
+        }
+    },
+    components: {
+        AdventurerGearDetails,
+        ItemCard
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~styles/shared.scss';
+
+</style>
