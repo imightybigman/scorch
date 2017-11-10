c110c1629344fe0133f856a8c73bb561bb3de3b8
tedens <edens.anthony@gmail.com>
2017-11-10 16:21:42 -0600

    part 2 of init rolling

---
+++ ScorchApp/src/components/dungeonMaster/DungeonMasterFight.vue
@@ -0,0 +1,119 @@
+<template>
+    <div class="dm-fight-component">
+        <div class="d-flex dm-character-view">
+            <div class="flex-column dm-party-view">
+                <div class="dm-character-cards-container" v-for="(char, index) in party" @click="toggleCharacter(char)" :key="index">
+                    <character-tile :character="char" v-bind:class="{ selected: isCharacterSelected(char) }"></character-tile>
+                </div>
+            </div>
+            <div class="rhs-character-ops">
+                <div class="d-flex bottom-bar">
+                    <div class="character-operations">
+                        <fight-operator :character-list="selectedChars"></fight-operator>
+                    </div>
+                </div>
+            </div>
+            <div class="d-flex">
+                <div class="flex-column">
+                    <div class="initiative">
+                        <div v-for="user in party">
+                            <ul>{{user.LastName}}: 0</ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+    import { CharacterTile } from 'components/character'
+    import FightOperator from './operators/FightOperator'
+
+export default {
+    name : 'dm-fight-component',
+    data() {
+      console.log(party);
+        return {
+            selectedChars: [],
+        }
+    },
+    async created() {
+      await this.$store.dispatch('getParty');
+    },
+    methods: {
+        toggleCharacter(character) {
+            var foundChar = this.selectedChars.find(char => char.CharacterId == character.CharacterId);
+
+            if(foundChar){
+                var index = this.selectedChars.findIndex(char => char.CharacterId == character.CharacterId);
+                this.selectedChars.splice(index, 1);
+            }
+            else{
+                this.selectedChars.push(character);
+            }
+        },
+        isCharacterSelected(character) {
+            return !(this.selectedChars.find(char => char.CharacterId == character.CharacterId) == undefined);
+        }
+    },
+    computed: {
+        party() {
+            return this.$store.getters.myParty;
+        }
+    },
+    components: {
+        CharacterTile,
+        FightOperator
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    div {
+        box-sizing: border-box;
+    }
+
+    .dm-main-component {
+        height: 600px;
+    }
+    .dm-character-view {
+        flex-wrap: wrap;
+    }
+    .dm-party-view {
+        margin: 1%;
+        flex: 1;
+        flex-grow: 2;
+        .dm-character-cards-container {
+            margin-bottom: 1%;
+            border-radius: 10px;
+        }
+    }
+    .character-operations {
+        margin: 1%;
+        flex: 1;
+        border-radius: 10px;
+    }
+    .selected {
+        background-color: black;
+    }
+    .item-searcher{
+        flex: 1;
+        flex-grow: 3;
+    }
+    .rhs-character-ops{
+        flex: 2;
+        flex-grow: 5;
+        flex-direction:column;
+    }
+    .bottom-bar{
+        flex-direction:row;
+    }
+    .item-card{
+        flex: 1;
+    }
+    .item-card-inner{
+        min-height: 75%;
+    }
+</style>
--- ScorchApp/src/components/dungeonMaster/index.js
+++ ScorchApp/src/components/dungeonMaster/index.js
@@ -2,10 +2,16 @@ import DungeonMasterMain from './DungeonMasterMain'
 import DungeonMasterStory from './DungeonMasterStory'
 import DungeonMasterInventoryManager from './DungeonMasterInventoryManager'
 import CharacterOperator from './operators/CharacterOperator'
+import DungeonMasterLogs from './DungeonMasterLogs'
+import DungeonMasterFight from './DungeonMasterFight'
+import FightOperator from './operators/FightOperator'
 
 export {
     DungeonMasterMain,
     DungeonMasterStory,
     DungeonMasterInventoryManager,
-    CharacterOperator
+    CharacterOperator,
+    DungeonMasterLogs,
+    DungeonMasterFight,
+    FightOperator
 }
