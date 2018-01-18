e412c3339066f694d1574e86e476a5b88e0908e3
jun2theizzle <junnobaka@gmail.com>
2018-01-17 19:04:24 -0600

    updated to use serverless api

--- ScorchApp/config/prod.env.js
+++ ScorchApp/config/prod.env.js
@@ -1,5 +1,5 @@
 module.exports = {
   NODE_ENV: '"production"',
-  API_HOST: '"https://dnd-api.imightybigman.com/api"',
+  API_HOST: '"https://dnd-api-prod.imightybigman.com/api"',
   SOCKET_HOST: '"https://socket.imightybigman.com:3000/"'
 }
